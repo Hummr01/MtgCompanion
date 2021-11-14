61644d8987e37711348cb500843626e4ef0c4adf
DevMetal <mail@dev-metal.net>
2021-11-14 14:23:26 +0100

    implement removeLife methods

--- shared/src/commonMain/kotlin/lifecounter/LifeCounter.kt
+++ shared/src/commonMain/kotlin/lifecounter/LifeCounter.kt
@@ -1,18 +1,29 @@
 package lifecounter
 
-class LifeCounter {
+class LifeCounter(private val currentLife: Int) {
+
     private fun addLife(amount: Int): Int {
-        // getCurrentLifeTotal
-        val newLifeTotal: Int = 0
-        return newLifeTotal + amount
+        return currentLife + amount
+    }
+
+    private fun removeLife(amount: Int): Int {
+        return currentLife - amount
     }
 
     fun addOneLife(): Int {
         return addLife(1)
     }
 
+    fun removeOneLife(): Int {
+        return removeLife(1)
+    }
+
     fun addFiveLife(): Int {
         return addLife(5)
+    }
+
+    fun removeFiveLife(): Int {
+        return removeLife(5)
     }
 
     fun setLifeTo(amount: Int): Int {
