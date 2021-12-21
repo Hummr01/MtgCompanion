3a4943e51900da5a51115f413c914942e1c441ee
Hummr <daniellaber@student.uni-kassel.de>
2021-12-21 20:27:34 +0100

    feat: add raiseBy
    Adding functionality to raise and decrease amount of counters in Counter class. Like mentioned in #4.

--- shared/src/commonMain/kotlin/de/mtgCompanion/shared/model/Counter.kt
+++ shared/src/commonMain/kotlin/de/mtgCompanion/shared/model/Counter.kt
@@ -6,4 +6,13 @@ class Counter  {
     // amount of counters
     internal var amount: Int = 0
 
+    // raises counters by given amount
+    fun raiseBy(addAmountBy : Int) {
+        this.amount += addAmountBy
+    }
+
+    // decrease counters by given amount
+    fun decreaseBy(decreaseAmountBy : Int) {
+        this.amount -= decreaseAmountBy
+    }
 }
