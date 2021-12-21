e94b2d780452a6976e022a5ebe820c1940349c88
Hummr <daniellaber@student.uni-kassel.de>
2021-12-21 21:51:47 +0100

    feat(constants): add constants for life amount

--- shared/src/commonMain/kotlin/de/mtgCompanion/shared/Constants.kt
+++ shared/src/commonMain/kotlin/de/mtgCompanion/shared/Constants.kt
@@ -1,9 +1,15 @@
 package de.mtgCompanion.shared
 
 object Constants {
+    // constants for colors
     const val PLAYER_COLOR_WHITE = "White"
     const val PLAYER_COLOR_BLACK = "Black"
     const val PLAYER_COLOR_RED = "Red"
     const val PLAYER_COLOR_GREEN = "Green"
     const val PLAYER_COLOR_BLUE = "Blue"
+
+    // constants for life amounts
+    const val LIFE_STANDARD = 20
+    const val LIFE_BRAWL = 30
+    const val LIFE_COMMANDER = 40
 }
