36ff86e5776d3d11f09f956863b03aae54ba0fd1
Hummr <daniellaber@student.uni-kassel.de>
2021-12-21 16:51:01 +0100

    chore: add constants for Player colors

---
+++ shared/src/commonMain/kotlin/de/mtgCompanion/shared/Constants.kt
@@ -0,0 +1,8 @@
+package de.mtgCompanion.shared
+
+object Constants {
+    private const val PLAYER_COLOR_WHITE = "White"
+    private const val PLAYER_COLOR_BLACK = "Black"
+    private const val PLAYER_COLOR_RED = "Red"
+    private const val PLAYER_COLOR_GREEN = "Green"
+}
